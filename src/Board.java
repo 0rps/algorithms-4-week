b4f33ab5aaedb2a2e33b744315f93d8bb029515e
MogilnikovAB <amogilnikov@oaorti.ru>
2017-07-14 16:43:54 +0300

    [upd] Some initialization

--- src/Board.java
+++ src/Board.java
@@ -2,15 +2,60 @@
  * Created by orps on 14.07.17.
  */
 public class Board {
-    public Board(int[][] blocks) {}
-    public int dimension() {}
-    public int hamming() {}
-    public int manhattan() {}
-    public boolean isGoal()  {}
-    public Board twin() {}
-    public boolean equals(Object y) {}
-    public Iterable<Board> neighbors() {}
-    public String toString() {}
 
-    public static void main(String[] args) {}
+    private final int[][] data;
+
+    public Board(int[][] blocks) {
+        if (blocks == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        final int rank = blocks.length;
+        for (int[] i: blocks) {
+            if (i.length != rank) {
+                throw new java.lang.IllegalArgumentException();
+            }
+        }
+
+        data = new int[blocks.length][];
+        for (int i = 0; i < rank; i++) {
+            data[i] = blocks[i].clone();
+        }
+    }
+
+    public int dimension() {
+        return data.length;
+    }
+
+    public int hamming() {
+        return 0;
+    }
+
+    public int manhattan() {
+        return 0;
+    }
+
+    public boolean isGoal()  {
+        return false;
+    }
+
+    public Board twin() {
+        return new Board(data);
+    }
+
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    public Iterable<Board> neighbors() {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -2,20 +2,27 @@
  * Created by orps on 14.07.17.
  */
 public class Solver {
-    public Solver(Board initial) {
 
+    private Board board;
+
+    public Solver(Board initial) {
+        if (initial == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        board = initial;
     }
 
     public boolean isSolvable() {
-
+        return false;
     }
 
     public int moves() {
-
+        return 0;
     }
 
     public Iterable<Board> solution() {
-
+        return board.neighbors();
     }
 
     public static void main(String[] args) {
